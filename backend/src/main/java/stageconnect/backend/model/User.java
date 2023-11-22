b68224ada31e6b544c63a08f8ac2567188d3a9f0
Salma <salmariahi26@gmail.com>
2023-11-22 08:50:47 +0100

    auth

--- backend/src/main/java/stageconnect/backend/model/User.java
+++ backend/src/main/java/stageconnect/backend/model/User.java
@@ -20,4 +20,44 @@ public class User {
     private String prenom;
     private String email;
     private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
