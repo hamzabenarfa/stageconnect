2b39ec6047de8f2fbb4c198358e993300add589d
hamza <hamzabenarfa4@gmail.com>
2023-12-15 04:51:48 +0100

    fixed

--- frontend/app/entreprise/_component/Navbar.tsx
+++ frontend/app/entreprise/_component/Navbar.tsx
@@ -1,13 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { Bell, ChevronDown, Search } from "lucide-react";
 import Image from "next/image";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between w-full rounded-3xl h-20 p-4  bg-white">
-      <h1 className="lg:text-3xl font-semibold capitalize">hi hamza!</h1>
+      <h1 className="lg:text-3xl font-semibold capitalize">hi Wiem!</h1>
 
       <div className="flex items-center gap-1">
-        <Search className="w-10 h-10 text-black p-2 rounded-full" />
-        <Bell className="w-10 h-10 text-black p-2 rounded-full" />
+        <Button variant="ghost" size="icon" className="rounded-full">
+          <Search className="w-10 h-10 text-black p-2 rounded-full" />
+        </Button>
+        <Button variant="ghost" size="icon" className="rounded-full">
+          <Bell className="w-10 h-10 text-black p-2 rounded-full" />
+        </Button>
+
         <Image
           src="/google.png"
           alt="user img"
@@ -16,7 +22,7 @@ const Navbar = () => {
           className="rounded-full"
         />
         <div className="flex items-center ml-1">
-          <p className="font-bold">Hamza Benarfa</p>
+          <p className="font-bold">Wiem Lmzara</p>
           <ChevronDown className="w-10 h-10 text-black p-2 rounded-full" />
         </div>
       </div>
